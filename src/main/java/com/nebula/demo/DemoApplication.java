b447ba73017f80b5349cb75fb95831f459bb5ccb
shitao <shitao.ca@gmail.com>
2018-08-03 15:02:51 -0400

    modify project package type to war.

--- src/main/java/com/nebula/demo/DemoApplication.java
+++ src/main/java/com/nebula/demo/DemoApplication.java
@@ -2,6 +2,8 @@ package com.nebula.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -10,10 +12,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 
 @SpringBootApplication
 //@EnableCaching
-public class DemoApplication {
+public class DemoApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(DemoApplication.class);
     }
 
 
